1e84a689d56cbff38294880518f789ac989ff18b
hidekiohnuma <heiwa0216@yahoo.co.jp>
2015-01-16 20:30:00 +0900

    sample

---
+++ LoadMoreTableView-master/LoadMoreTableViewTestTests/LoadMoreTableViewTestTests.h
@@ -0,0 +1,13 @@
+//
+//  LoadMoreTableViewTestTests.h
+//  LoadMoreTableViewTestTests
+//
+//  Created by hachinobu on 2013/08/02.
+//  Copyright (c) 2013年 hachinobu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LoadMoreTableViewTestTests : SenTestCase
+
+@end
